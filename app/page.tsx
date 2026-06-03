09ea7f085e5221d930f3be7f115a5fabea242da0
Arshiya Khayatzadeh <arshiyakhayatzadeh2006@gmail.com>
2026-06-03 10:47:55 -0400

    uploading

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
+"use client"
+import { useState } from "react"
 export default function Home(){
+  const [phase, setPhase] = useState("upload")
+  const [file,setFile] = useState(null)
   return(
     <main>
       <h1>Reume Roaster</h1>
+      {phase === "upload" && <div><p>upload your resume</p>
+      <input type="file" /></div>
+      }
+      {phase ==="processing"&& <p>roasting in progress</p>}
+      {phase === "done" && <p>here are your results</p>}
       </main>
   )
 }
