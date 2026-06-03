96fb3328969b248f2cd7b29f2eac985b2c3ff3bc
Arshiya Khayatzadeh <arshiyakhayatzadeh2006@gmail.com>
2026-06-03 19:48:22 -0400

    added route

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,27 @@
-"use client"
-import { useState } from "react"
-export default function Home(){
-  const [phase, setPhase] = useState("upload")
-  const [file,setFile] = useState(null)
-  return(
+"use client";
+import { useState } from "react";
+export default function Home() {
+  const [phase, setPhase] = useState("upload");
+  const [file, setFile] = useState<File | null>(null);
+  return (
     <main>
       <h1>Reume Roaster</h1>
-      {phase === "upload" && <div><p>upload your resume</p>
-      <input type="file" /></div>
-      }
-      {phase ==="processing"&& <p>roasting in progress</p>}
+      {phase === "upload" && (
+        <div>
+          <p>upload your resume</p>
+          <input
+            type="file"
+            onChange={(e) => {
+              setPhase("processing");
+              if (e.target.files !== null) {
+                setFile(e.target.files[0]);
+              }
+            }}
+          />
+        </div>
+      )}
+      {phase === "processing" && <p>roasting in progress</p>}
       {phase === "done" && <p>here are your results</p>}
-      </main>
-  )
+    </main>
+  );
 }
